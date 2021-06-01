390e1a5ecb553e8b83b6b04cd6ae1498cbdecc7e
divya395 <73489364+divya395@users.noreply.github.com>
2021-06-01 17:41:27 +0530

    Add files via upload

---
+++ Theme 4 with DB connection/contact.php
@@ -0,0 +1,41 @@
+<?php
+$username = filter_input(INPUT_POST, 'name');
+$email = filter_input(INPUT_POST, 'email');
+$message = filter_input(INPUT_POST, 'message');
+if (!empty($name)){
+if (!empty($email)){
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "test";
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
+}
+else{
+$sql = "INSERT INTO account (name, email, message)
+values ('$name','$email','$message')";
+if ($conn->query($sql)){
+echo "New record is inserted sucessfully";
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
+}
+else{
+echo "Password should not be empty";
+die();
+}
+}
+else{
+echo "Username should not be empty";
+die();
+}
+?>
